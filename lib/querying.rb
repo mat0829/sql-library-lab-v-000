556b0c461a407dc14cfdba0cd3ed9ec65ef224d0
David Anderson <matandersonlv@hotmail.com>
2019-05-14 19:01:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,9 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT `species`
-    FROM     `characters`
-    WHERE `species` = 'humans'
+  "SELECT `species` FROM `characters` WHERE `species` = 'humans'
     ORDER BY COUNT(*) DESC
     LIMIT    1; "
 end
