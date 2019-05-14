f9993fa152571f7558002c0ec3290e61c3b35ecc
David Anderson <matandersonlv@hotmail.com>
2019-05-14 16:03:47 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -31,3 +31,11 @@ CREATE TABLE characters (
   series_id INTEGER
 );
 
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
