f0b805463b13cad53729bdca7c8b529b3db7a68b
David Anderson <matandersonlv@hotmail.com>
2019-05-14 18:11:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT Series.title WHERE FROM series INNER JOIN characters on "
 end
 
 def select_character_names_and_number_of_books_they_are_in
