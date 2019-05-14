6e820f93868171ac018d68f9eff048a019ea2b44
David Anderson <matandersonlv@hotmail.com>
2019-05-14 16:18:47 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-"SET Characters.species FROM characters WHERE id=(SELECT MAX(id) FROM characters SETS = 'Martian';"
+"SET Characters.species FROM characters WHERE id=(SELECT MAX(id) FROM characters = 'Martian';"
