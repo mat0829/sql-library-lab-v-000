4f0a0a0977482f736cd78cd6cc1ffc6920658741
David Anderson <matandersonlv@hotmail.com>
2019-05-14 19:53:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,8 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT DISTINCT Characters.name, COUNT(Character_books.character_id) FROM characters,
-	 Character_books WHERE Character_books.character_id = Characters.id 
-	 GROUP BY Character_books.character_id ORDER BY COUNT(Character_books.character_id) 
-	 DESC, Characters.name ASC;"
+  "SELECT Characters.name, COUNT(Books.title) AS "count_occurence" FROM characters INNER JOIN character_books ON characters.id = character_books.character_id INNER JOIN books ON books.id = character_books.book_id GROUP BY characters.name HAVING COUNT(books.title) ORDER BY "count_occurence" DESC;"
 end
