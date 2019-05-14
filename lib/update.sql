743305163d74d35bbd4c2f5f6c5c8e995cc604a9
David Anderson <matandersonlv@hotmail.com>
2019-05-14 16:21:46 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,2 +1,2 @@
 "UPDATE Customers 
- SET ContactName='Juan';SET Characters.species FROM characters WHERE id=(SELECT MAX(id) FROM characters = 'Martian';"
+ SET ContactName='Juan';SET  FROM characters WHERE id=(SELECT MAX(id) FROM characters = 'Martian';"
