0fd2a6dcd989cfe7d8fb913a15fd1b02aa199071
David Anderson <matandersonlv@hotmail.com>
2019-05-14 17:42:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT Series.author_id, Series.subgenre_id FROM SERIES INNER JOIN AUTHORS ON Authors.id = Series.author_id INNER JOIN SUBGENRES ON Subgenres.id = Series.subgenre_id GROUP BY name, subgenre_id;"
+  "SELECT Series.author_id, Series.subgenre_id FROM SERIES INNER JOIN AUTHORS ON Authors.id = Series.author_id INNER JOIN SUBGENRES ON Subgenres.id = Series.subgenre_id GROUP BY author_id, subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
