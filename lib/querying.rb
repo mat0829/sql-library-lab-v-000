75633db5ba4ebf1744b72ca312b8df25eec46d22
David Anderson <matandersonlv@hotmail.com>
2019-05-14 19:19:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,8 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title SELECT * FROM Customers
-WHERE Country IN (SELECT Country FROM Suppliers);  = species('human');"
+  "SELECT Series.title;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
